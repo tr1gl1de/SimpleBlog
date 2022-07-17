2be2085e52df626905b2a79a1b03c9f00a68d6da
Max <pogatop@yandex.ru>
2022-07-17 20:29:37 +0300

    Add new method for UserRepos

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace Repositories;
 public interface IUserRepository
 {
     Task<User> GetUserByUsernameAsync(string username, CancellationToken cancellationToken = default);
+    Task<User> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken = default);
     void Insert(User user);
     void Remove(User user);
 }
