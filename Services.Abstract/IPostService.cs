edb24cff72d609d28553ca28f3b93d23a3e02b8a
Max <pogatop@yandex.ru>
2022-07-17 20:50:28 +0300

    Add post service interface

---
+++ Services.Abstract/IPostService.cs
@@ -0,0 +1,22 @@
+﻿using Contracts.PostDto;
+
+namespace Services.Abstract;
+
+public interface IPostService
+{
+    Task<PostForReadDto> CreatePostAsync(PostForCreationDto postForCreation, CancellationToken cancellationToken = default);
+    Task<PostForReadDto> GetPostByIdAsync(Guid postId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<PostForReadDto>> GetAllPostsAsync(CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<PostForReadDto>> GetPostsByUsernameAsync(string username,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<PostForReadDto>> GetPostsByTagsAsync(List<string> tags,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<PostForReadDto>> GetPostsByNameAsync(string name, CancellationToken cancellationToken = default);
+
+    Task UpdatePostByIdAsync(Guid postId, PostForUpdateDto postForUpdate, CancellationToken cancellationToken = default);
+
+    Task DeletePostByIdAsync(Guid postId, CancellationToken cancellationToken = default);
+}
