8519120452ee8c5a2e9fd30dce79aa7f9e6d6652
Max <pogatop@yandex.ru>
2022-07-17 21:25:06 +0300

    Add user service interface

---
+++ Services.Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Contracts.UserDto;
+
+namespace Services.Abstract;
+
+public interface IUserService
+{
+    Task<UserForReadDto> RegisterUserAsync(UserForRegDto userForReg, CancellationToken cancellationToken = default);
+    Task<UserForReadDto> AuthenticateUserAsync(UserForLoginDto userForLogin, CancellationToken cancellationToken = default);
+    Task<UserForReadDto> GetUserByUsername(string username, CancellationToken cancellationToken = default);
+
+    Task UpdateUserByUsername(string username, UserForUpdateDto userForUpdate,
+        CancellationToken cancellationToken = default);
+}
