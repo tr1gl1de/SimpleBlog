47e1522a4c2f3298558dd764189a8c2c3971f367
Max <pogatop@yandex.ru>
2022-07-18 16:10:34 +0300

    Add comment service interface

---
+++ Services.Abstract/ICommentService.cs
@@ -0,0 +1,23 @@
+﻿using Contracts.CommentDto;
+
+namespace Services.Abstract;
+
+public interface ICommentService
+{
+    Task<CommentForCreationDto> CreateCommentAsync(Guid postId, Guid? parentCommentId = null,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CommentForReadDto>> GetCommentsByUsernameAsync(string username,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CommentForReadDto>>
+        GetCommentsByPostId(Guid postId, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<CommentForReadDto>> GetChildrenCommentsByIdCommentAsync(Guid commentId,
+        CancellationToken cancellationToken = default);
+
+    Task UpdateCommentByIdAsync(Guid commentId, CommentForUpdateDto commentForUpdate,
+        CancellationToken cancellationToken = default);
+
+    Task DeleteCommentByIdAsync(Guid commentId, CancellationToken cancellationToken = default);
+}
