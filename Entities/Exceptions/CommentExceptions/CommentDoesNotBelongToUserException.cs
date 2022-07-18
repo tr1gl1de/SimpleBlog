d637ce7d7f5473e965dd2affcf10fc6d179b38a5
Max <pogatop@yandex.ru>
2022-07-18 17:30:36 +0300

    Add comment exceptions

---
+++ Entities/Exceptions/CommentExceptions/CommentDoesNotBelongToUserException.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.Exceptions.CommentExceptions;
+
+public class CommentDoesNotBelongToUserException : BadRequestException
+{
+    public CommentDoesNotBelongToUserException(Guid commentId, Guid userId) : 
+        base($"The comment with identifier \"{commentId}\" does not belong to user with identifier \"{userId}\"")
+    {
+    }
+    
+    public CommentDoesNotBelongToUserException(Guid commentId, string username) : 
+        base($"The comment with identifier \"{commentId}\" does not belong to user with username \"{username}\"")
+    {
+    }
+}
---
+++ Entities/Exceptions/CommentExceptions/CommentNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions.CommentExceptions;
+
+public class CommentNotFoundException : NotFoundException
+{
+    public CommentNotFoundException(Guid commentId) : 
+        base($"Comment with identifier \"{commentId}\" was not found.")
+    {
+    }
+}
