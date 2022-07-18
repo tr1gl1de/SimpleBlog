89e9b56d5f664edb0c3c43b860ec384acad1bdf0
Max <pogatop@yandex.ru>
2022-07-18 17:16:05 +0300

    Add post exceptions

---
+++ Entities/Exceptions/PostExceptions/PostDoesNotBelongToUserException.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.Exceptions.PostExceptions;
+
+public class PostDoesNotBelongToUserException : BadRequestException
+{
+    public PostDoesNotBelongToUserException(Guid postId, Guid userId) : 
+        base($"The post with identifier \"{postId}\" does not belong to the user with the identifier \"{userId}\".")
+    {
+    }
+    
+    public PostDoesNotBelongToUserException(Guid postId, string username) : 
+        base($"The post with identifier \"{postId}\" does not belong to the user with the username \"{username}\".")
+    {
+    }
+}
---
+++ Entities/Exceptions/PostExceptions/PostNotFoundExceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Exceptions.PostExceptions;
+
+public sealed class PostNotFoundExceptions : NotFoundException
+{
+    public PostNotFoundExceptions(string name) : 
+        base($"Post with name \"{name}\" was not found.")
+    {
+    }
+    
+    public PostNotFoundExceptions(Guid postId) : 
+        base($"Post with identifier \"{postId}\" was not found.")
+    {}
+}
