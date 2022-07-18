204f96a96f33e090ecb57a166e214cd091657d53
Max <pogatop@yandex.ru>
2022-07-18 16:19:13 +0300

    Add service manager interface

---
+++ Services.Abstract/IServiceManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Abstract;
+
+public interface IServiceManager
+{
+    ICommentService CommentService { get; }
+    IPostService PostService { get; }
+    IUserService UserService { get; }
+}
