3282ea15bc060ac18dfe6414a152047ad9d9a976
Max <pogatop@yandex.ru>
2022-07-19 18:40:59 +0300

    Add post service implementation

---
+++ Services/PostService.cs
@@ -0,0 +1,60 @@
+﻿using Contracts.PostDto;
+using Entities.Models;
+using Mapster;
+using Repositories;
+using Services.Abstract;
+
+namespace Services;
+
+public class PostService : IPostService
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public PostService(IRepositoryManager repositoryManager)
+    {
+        _repositoryManager = repositoryManager;
+    }
+    
+    public async Task<PostForReadDto> CreatePostAsync(PostForCreationDto postForCreation, CancellationToken cancellationToken = default)
+    {
+        var post = postForCreation.Adapt<Post>();
+        _repositoryManager.PostRepository.Insert(post);
+        await _repositoryManager.UnitOfWork.SaveChangesAsync(cancellationToken);
+        return post.Adapt<PostForReadDto>();
+    }
+
+    public Task<PostForReadDto> GetPostByIdAsync(Guid postId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PostForReadDto>> GetAllPostsAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PostForReadDto>> GetPostsByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PostForReadDto>> GetPostsByTagsAsync(List<string> tags, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PostForReadDto>> GetPostsByNameAsync(string name, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdatePostByIdAsync(Guid postId, PostForUpdateDto postForUpdate, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeletePostByIdAsync(Guid postId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
