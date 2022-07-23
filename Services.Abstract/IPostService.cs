3d27908026b4d5858560766a40df72d7e934819c
Maxim <pogatop@yandex.ru>
2022-07-24 00:13:03 +0300

    Add arg username for command methods in IPostService

--- Services.Abstract/IPostService.cs
+++ Services.Abstract/IPostService.cs
@@ -4,7 +4,7 @@ namespace Services.Abstract;
 
 public interface IPostService
 {
-    Task<PostForReadDto> CreatePostAsync(PostForCreationDto postForCreation, CancellationToken cancellationToken = default);
+    Task<PostForReadDto> CreatePostAsync(PostForCreationDto postForCreation, string usernameCreator, CancellationToken cancellationToken = default);
     Task<PostForReadDto> GetPostByIdAsync(Guid postId, CancellationToken cancellationToken = default);
     Task<IEnumerable<PostForReadDto>> GetAllPostsAsync(CancellationToken cancellationToken = default);
 
@@ -16,7 +16,7 @@ public interface IPostService
 
     Task<IEnumerable<PostForReadDto>> GetPostsByNameAsync(string name, CancellationToken cancellationToken = default);
 
-    Task UpdatePostByIdAsync(Guid postId, PostForUpdateDto postForUpdate, CancellationToken cancellationToken = default);
+    Task UpdatePostByIdAsync(Guid postId, string usernameCreator,PostForUpdateDto postForUpdate, CancellationToken cancellationToken = default);
 
-    Task DeletePostByIdAsync(Guid postId, CancellationToken cancellationToken = default);
+    Task DeletePostByIdAsync(Guid postId, string usernameCreator,CancellationToken cancellationToken = default);
 }
