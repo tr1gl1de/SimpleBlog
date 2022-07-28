4386d2e7e93ddd43f787168f121d3f55c370403c
Maxim <pogatop@yandex.ru>
2022-07-28 16:52:32 +0300

    Add validator for UserForReadDto

--- EntityValidators/ValidatorManager.cs
+++ EntityValidators/ValidatorManager.cs
@@ -1,8 +1,10 @@
 ﻿using EntityValidators.Validators.PostValidators;
+using EntityValidators.Validators.UserValidators;
 
 namespace EntityValidators;
 
 public class ValidatorManager
 {
     public PostForCreateDtoValidator PostForCreateValidator { get; } = new();
+    public UserForReadDtoValidator UserForReadValidator { get; } = new();
 }
---
+++ EntityValidators/Validators/UserValidators/UserForReadDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using Contracts.UserDto;
+using FluentValidation;
+
+namespace EntityValidators.Validators.UserValidators;
+
+public class UserForReadDtoValidator : AbstractValidator<UserForReadDto>
+{
+    public UserForReadDtoValidator()
+    {
+        RuleFor(u => u.Username).NotNull().MinimumLength(3).MaximumLength(10);
+        RuleFor(u => u.Displayname).NotNull().MinimumLength(3).MaximumLength(20);
+    }
+}
