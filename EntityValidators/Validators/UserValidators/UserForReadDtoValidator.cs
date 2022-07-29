105c400daace6b6c6138d2c501e9cd78d6984f6e
Maxim <pogatop@yandex.ru>
2022-07-30 01:01:49 +0300

    Refactor: change the maximum of lenght the UserForRead validator to a large side

--- EntityValidators/Validators/UserValidators/UserForReadDtoValidator.cs
+++ EntityValidators/Validators/UserValidators/UserForReadDtoValidator.cs
@@ -7,7 +7,7 @@ public class UserForReadDtoValidator : AbstractValidator<UserForReadDto>
 {
     public UserForReadDtoValidator()
     {
-        RuleFor(u => u.Username).NotNull().MinimumLength(3).MaximumLength(20);
-        RuleFor(u => u.Displayname).NotNull().MinimumLength(3).MaximumLength(20);
+        RuleFor(u => u.Username).NotNull().MinimumLength(3).MaximumLength(25);
+        RuleFor(u => u.Displayname).NotNull().MinimumLength(2).MaximumLength(30);
     }
 }
