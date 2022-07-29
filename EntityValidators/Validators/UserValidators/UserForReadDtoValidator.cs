79d964c3ae6f73c448567a8eaf3fa3334c827eb0
Maxim <pogatop@yandex.ru>
2022-07-29 17:54:20 +0300

    Fix: change the maximum length of the validator to a large side

--- EntityValidators/Validators/UserValidators/UserForReadDtoValidator.cs
+++ EntityValidators/Validators/UserValidators/UserForReadDtoValidator.cs
@@ -7,7 +7,7 @@ public class UserForReadDtoValidator : AbstractValidator<UserForReadDto>
 {
     public UserForReadDtoValidator()
     {
-        RuleFor(u => u.Username).NotNull().MinimumLength(3).MaximumLength(10);
+        RuleFor(u => u.Username).NotNull().MinimumLength(3).MaximumLength(20);
         RuleFor(u => u.Displayname).NotNull().MinimumLength(3).MaximumLength(20);
     }
 }
