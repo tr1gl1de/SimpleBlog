161a0d5e257a0a0454365fe8fadfc287dcb5224c
Max <pogatop@yandex.ru>
2022-07-31 00:29:21 +0300

    Add test and test cases for GetPostById method

---
+++ Tests/ServicesTests/PostServiceTests/GetPostByIdTests/GetPostByIdDataTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.ServicesTests.PostServiceTests.GetPostByIdTests;
+
+internal class GetPostByIdDataTests
+{
+    private static readonly IEnumerable<Guid> _validGuids = GeneratorValidGuid(3);
+    
+    private static IEnumerable<Guid> GeneratorValidGuid(int lenght)
+    {
+        var result = new List<Guid>();
+        
+        for (int i = 0; i < lenght; i++)
+        {
+            result.Add(Guid.NewGuid());
+        }
+
+        return result;
+    }
+
+    public static IEnumerable<object[]> GetValidGuids()
+    {
+        foreach (var validGuid in _validGuids)
+        {
+            yield return new object[]
+            {
+                validGuid
+            };
+        }
+    }
+}
---
+++ Tests/ServicesTests/PostServiceTests/GetPostByIdTests/GetPostByIdTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Contracts.PostDto;
+using Entities.Models;
+using EntityValidators;
+using Moq;
+using Repositories;
+using Services;
+using Services.Abstract;
+using Xunit;
+
+namespace Tests.ServicesTests.PostServiceTests.GetPostByIdTests;
+
+public class GetPostByIdTests
+{
+    private readonly IPostService _postService;
+    private readonly Mock<IRepositoryManager> _mockRepos;
+
+
+    public GetPostByIdTests()
+    {
+        _mockRepos = new Mock<IRepositoryManager>();
+        var validatorManager = new ValidatorManager();
+        _postService = new PostService(_mockRepos.Object, validatorManager);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPostByIdDataTests.GetValidGuids), MemberType = typeof(GetPostByIdDataTests))]
+    public async Task GetPostById_InputValidGuid_ReturnPostForRead(Guid testGuid)
+    {
+        // Arrange
+        var expectedPostForRead = new PostForReadDto()
+        {
+            Name = "test",
+            Text = "Some text",
+            UsernameCreator = "username",
+        };
+        var returnedPost = new Post()
+        {
+            Id = testGuid,
+            Name = expectedPostForRead.Name,
+            Text = expectedPostForRead.Text,
+            UsernameCreator = expectedPostForRead.UsernameCreator
+        };
+        _mockRepos.Setup(manager => manager.PostRepository.GetPostByIdAsync(testGuid, default))
+            .ReturnsAsync(returnedPost);
+        // Act
+        var result = await _postService.GetPostByIdAsync(testGuid);
+        // Assert
+        Assert.Equal(expectedPostForRead, result);
+    }
+}
