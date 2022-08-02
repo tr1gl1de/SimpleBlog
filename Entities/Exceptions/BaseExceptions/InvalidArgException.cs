0e5f6c927b60094c332289758115cb22f8bd89f2
Max <pogatop@yandex.ru>
2022-08-02 17:48:16 +0300

    Add exception for empty guid value

---
+++ Entities/Exceptions/BaseExceptions/InvalidArgException.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Exceptions.BaseExceptions;
+
+public class InvalidArgException : Exception
+{
+    protected InvalidArgException(string message) : base(message)
+    {}
+}
---
+++ Entities/Exceptions/CustomExceptions/PostExceptions/PostNotValidGuidException.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Exceptions.BaseExceptions;
+
+namespace Entities.Exceptions.CustomExceptions.PostExceptions;
+
+public class PostNotValidGuidException : InvalidArgException
+{
+    public PostNotValidGuidException()
+        : base($"Guid can not be empty")
+    {
+    }
+}
