b2d8705453d06740bd1e56d22284beb7c620f926
Max <pogatop@yandex.ru>
2022-08-03 17:17:41 +0300

    Refactor: remove the unused field and add some fields for CommentForReadDto

--- Contracts/CommentDto/CommentForReadDto.cs
+++ Contracts/CommentDto/CommentForReadDto.cs
@@ -2,10 +2,12 @@
 
 public class CommentForReadDto
 {
+    public Guid CommentId { get; set; }
     public string UsernameCreator { get; set; } = string.Empty;
     public DateTime CreateDate { get; set; }
     public DateTime? UpdateDate { get; set; }
     public string Text { get; set; } = string.Empty;
     public int LikesCount { get; set; }
-    public int CommentsCount { get; set; }
+    public Guid? ParentCommentId { get; set; }
+    public List<CommentForReadDto>? ChildrenComments { get; set; }
 }
