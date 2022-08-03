250dad207b25f61925157e60add9b668c9aaacf3
Max <pogatop@yandex.ru>
2022-08-03 17:15:22 +0300

    Add init for filed in Comment entity.

--- Entities/Models/Comment.cs
+++ Entities/Models/Comment.cs
@@ -5,7 +5,7 @@ public class Comment
     public Guid Id { get; set; }
     public Guid PostId { get; set; }
     public string UsernameCreator { get; set; } = string.Empty;
-    public DateTime CreateDate { get; set; }
+    public DateTime CreateDate { get; set; } = DateTime.UtcNow;
     public DateTime? UpdateDate { get; set; }
     public string Text { get; set; } = string.Empty;
     public int LikesCount { get; set; } = 0;
